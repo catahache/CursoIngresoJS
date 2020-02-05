a0d0fcb43a5d7af86ca2dd931caa15c6eecb75c4
Cata Hache <47184235+catahache@users.noreply.github.com>
2020-02-05 10:36:42 -0300

    Ejercicio 8 resuelto
    resuelto

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,21 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	//modulo %
+	var dividendo;
+	var divisor;
+	var resultado;
+
+	dividendo = document.getElementById('numeroDividendo').value;
+	console.log("Ingreso: " + dividendo);
+	primerNumero = parseInt(dividendo);
+	divisor = document.getElementById('numeroDivisor').value;
+	console.log("Ingreso: " + divisor);
+	primerNumero = parseInt(divisor);
+
+	resultado = dividendo % divisor;
+
+	console.log(resultado); 
+
 	
 }
