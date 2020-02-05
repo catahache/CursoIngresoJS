eb0ee6ffd21cdc574140322de6ecf6d2c871a5cb
Cata Hache <47184235+catahache@users.noreply.github.com>
2020-02-05 11:08:07 -0300

    Ejercicio 9 resuelto

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,20 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var resultado;
+
+	sueldo = document.getElementById('sueldo').value;
+	sueldo = parseInt(sueldo);
+
+	resultado = sueldo * 1.10;
+
+	document.getElementById('resultado').value = resultado;
+	resultado = parseInt(resultado);
+
+	
+	console.log("El sueldo con aumento es: " + resultado);
+
 
 	
 }
