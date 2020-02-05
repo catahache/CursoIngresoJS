3d2ebf60734c639036103e9c79c7dd259a9473b9
Cata Hache <47184235+catahache@users.noreply.github.com>
2020-02-05 11:23:03 -0300

    Ejercicio 10 resuelto

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var importe;
+	var porciento25;
+	var resultado;
+
+	importe = document.getElementById('importe').value;
+	importe = parseInt(importe);
+
+	porciento25 = ( 25 * importe) / 100;
+	resultado = importe - porciento25;
+
+	document.getElementById('resultado').value = resultado;
+
+	console.log("El importe con descuento es: " + resultado);
+
 	
 }
