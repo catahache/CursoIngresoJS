1321b03e433b3fc9e303d57d156690657f9b08ce
alumno <alumno@lab6pc00>
2020-02-15 12:43:19 -0300

    parciales del 1 al 4 resultos

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,40 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
 
+	numeroUno = prompt ("Ingrese el primer numero");
+	numeroDos = prompt ("Ingrese el segundo numero");
+
+	if(numeroUno == numeroDos)
+	{
+		resultado = numeroUno + numeroDos;
+	}
+	else
+	{
+		if (numeroUno > numeroDos)
+		{
+			resultado = parseInt(numeroUno) - parseInt (numeroDos);
+		}
+		else
+		{
+			resultado = parseInt(numeroUno) + parseInt (numeroDos);
+		}
+	}
+	
+	if (resultado > 10)
+	{
+		alert("La suma es "+ resultado +" y supero el 10");
+	}
+	else
+	{
+		alert ("El resultado es "+ resultado);
+	}
+
+	
+
+	
 }
+
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
+//Pedir por prompt el precio y el porcentaje de 
+//descuento, mostrar el precio final con descuento 
+//por id.
 function mostrar()
 {
+	var precio;
+	var porcentajeDeDescuento;
+	var precioFinal;
 
+	precio = prompt ("Ingrese precio.");
+	porcentajeDeDescuento = prompt ("Ingrese el porcentaje de descuento.");
+
+	precioFinal = precio - precio*porcentajeDeDescuento/100;
+
+	document.getElementById('elPrecioFinal').value = precioFinal;
 }
