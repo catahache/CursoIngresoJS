824b74f44baa237518c10dabff41a4dd287057c9
Cata Hache <herediacatalina1995@gmail.com>
2020-02-27 22:49:31 -0300

    resolver parcial 8

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,6 +1,26 @@
-function NumerosPares()
+function mostrar() //RESOLVER
 {
-	var letraIngresada;
 	var numeroIngresado;
-	
+	var letraIngresada;
+	var respuesta;
+
+	respuesta = prompt("Ingresar 'si' para ingresar letra y numero");
+
+	for(respuesta = 'si'; respuesta === 'si'; )
+	{
+		letraIngresada = prompt("Ingresar letra");
+
+		for(letraIngresada=letraIngresada; !isNaN(letraIngresada); )
+		{
+			console.log("Letra ingresada: " + letraIngresada);
+		}
+
+		numeroIngresado = prompt("Ingresar número;");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		for(numeroIngresado=numeroIngresado; numeroIngresado > -100 && numeroIngresado < 100; )
+		{
+			console.log("Numero ingresado: " + numeroIngresado);
+		}
+	}
 }
