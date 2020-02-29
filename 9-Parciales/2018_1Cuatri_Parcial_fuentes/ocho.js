13d08f903e0d859ccf04b28d874bd4c8c5c3d9f5
Cata Hache <herediacatalina1995@gmail.com>
2020-02-29 16:05:02 -0300

    Parcial 8 terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -3,11 +3,129 @@ function mostrar() //RESOLVER
 	var numeroIngresado;
 	var letraIngresada;
 	var respuesta;
+	var numeroPar;
+	var cantidadPares;
+	var numeroImpar;
+	var cantidadImpares;
+	var cero;
+	var cantidadCeros;
+	var cantidadPositivos;
+	var sumaPositivos;
+	var sumaNegativos;
+	var contador;
+	var minimo;
+	var maximo;
+	var letraMinima;
+	var letraMaxima;
 
-	respuesta = prompt("Ingresar 'si' para ingresar letra y numero");
+	
+	respuesta = "si";
+	cantidadPares = 0;
+	cantidadImpares = 0;
+	cantidadCeros = 0;
+	sumaPositivos = 0;
+	cantidadPositivos = 0;
+	sumaNegativos = 0;
+	contador = 0;
+	
 
-	for(respuesta = 'si'; respuesta === 'si'; )
+	while(respuesta === "si")
 	{
+		letraIngresada = prompt ("Ingrese un letra");
+		while(!isNaN(letraIngresada))
+		{
+			letraIngresada = prompt ("Ingrese una letra");
+		}
+
+		numeroIngresado = prompt ("Ingrese un numero");
+		numeroIngresado = parseInt (numeroIngresado);
+
+		while(isNaN(numeroIngresado) || numeroIngresado< -100 || numeroIngresado > 100)
+		{
+			numeroIngresado = prompt ("Ingrese un numero");
+		}
+
+		contador++;
+
+		//Cantidad de pares
+		if(numeroIngresado%2 == 0 && numeroIngresado != 0)
+		{
+			cantidadPares++;
+		}
+		else // Cantidad de ceros
+		{
+			if(numeroIngresado == 0)
+			{
+				cantidadCeros++;
+			}
+			else //Cantidad de impares
+			{
+				cantidadImpares++;
+			}
+		}
+
+		//Cantidad de positivos
+		if(numeroIngresado> -1) 
+		{
+			sumaPositivos = sumaPositivos + numeroIngresado;
+			cantidadPositivos++;
+		}
+		else //Suma de positivos
+		{
+			if(numeroIngresado< 0)
+			{
+				sumaNegativos = sumaNegativos + numeroIngresado;
+			}
+		}
+
+		//Maximo y minimo
+		if(contador == 1) 
+		{
+			maximo = numeroIngresado;
+			minimo = numeroIngresado;
+			letraMaxima = letraIngresada;
+			letraMinima = letraIngresada;
+		}
+		else
+		{
+			if(numeroIngresado > maximo)
+			{
+				maximo = numeroIngresado;
+				letraMaxima = letraIngresada;
+			}
+			else
+			{
+				if (numeroIngresado < minimo)
+				{
+					minimo = numeroIngresado;
+					letraMinima = letraIngresada;
+				}
+			}
+		}	
+
+		respuesta = prompt ("Ingrese 'si' para ingresar letra y numero.")
+	}
+
+	promedio = sumaPositivos / cantidadPositivos;
+
+	console.log("Cantidad numeros pares: " + cantidadPares);
+	console.log("Cantidad numeros impares: " + cantidadImpares);
+	console.log("Cantidad de ceros: " + cantidadCeros);
+	console.log("Promedio numeros positivos: " + promedio);
+	console.log("Suma negativos: " + sumaNegativos);
+	console.log("Letra máxima y número máximo " + maximo + letraMaxima);
+	console.log("Letra mínima y número mínimo "+ minimo + letraMinima);
+
+
+
+
+
+
+
+
+	/*for(respuesta = 'si'; respuesta === 'si'; )
+	{
+		respuesta = prompt("Ingresar 'si' para ingresar letra y numero");
 		letraIngresada = prompt("Ingresar letra");
 
 		for(letraIngresada=letraIngresada; !isNaN(letraIngresada); )
@@ -22,5 +140,6 @@ function mostrar() //RESOLVER
 		{
 			console.log("Numero ingresado: " + numeroIngresado);
 		}
-	}
+
+	}*/
 }
